14ff79e9f123e053a91d3fe70d9dfe2afef9c03d
VitalyiKaryakin <karyakin01@mail.ru>
2024-12-28 09:51:09 +0300

    Домашнее задание по теме "Файлы в операционной системе"

---
+++ main.py
@@ -0,0 +1,61 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board):
+    # Проверка строк
+    for row in board:
+        if row[0] == row[1] == row[2] != " ":
+            return row[0]
+
+    # Проверка столбцов
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != " ":
+            return board[0][col]
+
+    # Проверка диагоналей
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
+
+    return None
+
+def is_board_full(board):
+    for row in board:
+        if " " in row:
+            return False
+    return True
+
+def tic_tac_toe():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        row = int(input(f"Игрок {current_player}, введите номер строки (0, 1, 2): "))
+        col = int(input(f"Игрок {current_player}, введите номер столбца (0, 1, 2): "))
+
+        if board[row][col] != " ":
+            print("Эта клетка уже занята. Попробуйте снова.")
+            continue
+
+        board[row][col] = current_player
+
+        winner = check_winner(board)
+        if winner:
+            print_board(board)
+            print(f"Поздравляем! Игрок {winner} выиграл!")
+            break
+
+        if is_board_full(board):
+            print_board(board)
+            print("Игра окончена вничью!")
+            break
+
+        current_player = "O" if current_player == "X" else "X"
+
+if __name__ == "__main__":
+    tic_tac_toe()
+
